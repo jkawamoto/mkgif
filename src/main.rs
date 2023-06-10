afe2b660931ec11c86a56a65b6deef2e1a094d9e
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-06-10 00:03:26 -0600

    use clap to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-use std::env;
 use std::fs::File;
+use std::string::ToString;
 
 use anyhow::Result;
+use clap::Parser;
 use image::codecs::gif::{GifEncoder, Repeat};
 use image::io::Reader;
 use image::Frame;
 
-fn main() -> Result<()> {
-    let mut paths: Vec<_> = env::args().skip(1).collect();
-    paths.sort();
+const DEFAULT_OUTPUT: &str = "output.gif";
 
-    let mut out = GifEncoder::new_with_speed(File::create("anime.gif")?, 5);
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the output file [default: output.gif].
+    #[arg(short, long, value_name = "FILE")]
+    output: Option<String>,
+    /// Paths to the input image files.
+    paths: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let mut args = Args::parse();
+    args.paths.sort();
+
+    let mut out = GifEncoder::new(File::create(
+        args.output.unwrap_or(DEFAULT_OUTPUT.to_string()),
+    )?);
     out.set_repeat(Repeat::Infinite)?;
-    for p in paths {
+    for p in args.paths {
         let img = Reader::open(p)?.decode()?;
         out.encode_frame(Frame::new(img.into_rgba8()))?;
     }
