5078163d96bc4b85ae3ff4162fa26367f1851ee4
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-06-10 20:17:55 -0600

    add speed param

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ struct Args {
     /// Path to the output file [default: output.gif].
     #[arg(short, long, value_name = "FILE")]
     output: Option<String>,
+    /// Processing speed in [1, 30]. The higher the value the faster it runs at the cost of image quality.
+    #[arg(short, long, value_name = "SPEED", default_value_t = 10)]
+    speed: i32,
     /// Paths to the input image files.
     paths: Vec<String>,
 }
@@ -25,9 +28,10 @@ fn main() -> Result<()> {
     args.paths.sort();
 
     let pb = ProgressBar::new(args.paths.len() as u64);
-    let mut out = GifEncoder::new(File::create(
-        args.output.unwrap_or(DEFAULT_OUTPUT.to_string()),
-    )?);
+    let mut out = GifEncoder::new_with_speed(
+        File::create(args.output.unwrap_or(DEFAULT_OUTPUT.to_string()))?,
+        args.speed,
+    );
     out.set_repeat(Repeat::Infinite)?;
     for p in args.paths {
         let img = Reader::open(p)?.decode()?;
